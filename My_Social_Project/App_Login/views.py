161bc02f54db367e4dacf7b9adc3a7e4ad089f1a
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-06-12 12:39:56 +0600

    completed user profile

--- My_Social_Project/App_Login/views.py
+++ My_Social_Project/App_Login/views.py
@@ -30,7 +30,7 @@ def login_page(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                pass
+                return HttpResponseRedirect(reverse('App_Posts:home'))
 
     return render(request, 'App_Login/login.html', context={'title': 'Login', 'form': form})
 
@@ -45,3 +45,8 @@ def edit_profile(request):
     else:
         form = EditProfile(instance=current_user)
     return render(request, 'App_Login/profile.html', context={'form': form, 'title': 'Edit Profile .Social'})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('App_Login:login'))
--- My_Social_Project/App_Posts/views.py
+++ My_Social_Project/App_Posts/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(request):
-    return HttpResponse("Hoemepage")
+    return render(request, 'App_Posts/home.html', context={'title': 'Home'})
